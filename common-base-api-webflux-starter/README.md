41a430e4ff9e6f5a9da7659ef40a38111fe2597d
Казачков Евгений <evgeniy.kazachkov@gmail.com>
2025-10-17 16:05:34 +0300

    init

---
+++ common-base-api-webflux-starter/README.md
@@ -0,0 +1,14 @@
+# Модуль настроек сваггера и общих контроллеров для WebFlux
+
+#№ Подключение библиотеки
+
+1. Добавить зависимости в pom.xml
+
+```xml
+
+<dependency>
+  <groupId>ru.sbrf.ufs.common.cssru.sbrf.ufs.common.css</groupId>
+  <artifactId>common-base-api-webflux-starter</artifactId>
+  <version>${common.ssp}</version>
+</dependency>
+```
---
+++ common-base-api-webflux-starter/src/main/java/ru/sbrf/ufs/common/css/base/api/webflux/configurations/AutoConfiguration.java
@@ -0,0 +1,14 @@
+package ru.sbrf.ufs.common.css.base.api.webflux.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(
+    basePackages = {
+        "ru.sbrf.ufs.common.css.base.api.webflux"
+    }
+)
+public class AutoConfiguration {
+
+}
